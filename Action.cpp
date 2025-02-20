f100f774ff428c6ee3a7b0d543c4a5bcae8f0eac
Lauradshm <dshmlaura@gmail.com>
2025-02-20 11:12:55 +0100

    Add files via upload

--- Action.cpp
+++ Action.cpp
@@ -1,13 +1,5 @@
 #include "Action.h"
-class Action {
-public:
-	virtual void onNewAircraft(Aircraft* aircraft) = 0;
 
-	void update(Subject* subjekt) {
-
-	};
-
-protected:
-	virtual void internalUpdate(Aircraft* aircraft) = 0;
-};
-};
+void Action::update(Subject* subjekt)
+{
+}
--- Action.h
+++ Action.h
@@ -7,9 +7,7 @@ class Action
 public:
 	virtual void onNewAircraft(Aircraft* aircraft) = 0;
 	
-	void update(Subject* subjekt) {
-		
-	};
+	void update(Subject* subjekt);
 
 protected:
 	virtual void internalUpdate(Aircraft* aircraft) = 0;
