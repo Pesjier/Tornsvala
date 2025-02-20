27c420040d3a61e4756eeb89c0a09c0c9dbfff23
Lauradshm <dshmlaura@gmail.com>
2025-02-20 11:38:12 +0100

    Add files via upload

--- ActionResponse.h
+++ ActionResponse.h
@@ -1,11 +1,12 @@
 #pragma once
 #include<vector>
+using namespace std;
 class Aircraft;
 class Action;
 class ActionResponse
 {
 private:
-	
+	vector<Action> actions;
 public:
 	void onNewAircraft(Aircraft& aircraft);
 
