4fd510227c6ffb805e9ee43408fa217cdf7bbafc
Lauradshm <dshmlaura@gmail.com>
2025-02-20 12:57:26 +0100

    Add files via upload

--- AircraftCollection.cpp
+++ AircraftCollection.cpp
@@ -2,29 +2,57 @@
 
 void AircraftCollection::add(string tailcode, string type)
 {
+
+
 }
 
-void AircraftCollection::addAircraft(Aircraft* aircraft)
+void AircraftCollection::addAircraft(Aircraft* craft)
 {
+	for (int i = 0; i < aircraft.size(); i++) {
+		if (aircraft[i] != craft) {
+			aircraft.push_back(craft);
+			break;
+		}
+	}
 }
 
 void AircraftCollection::remove(string tailcode)
 {
+	for (int i = 0; i < aircraft.size(); i++) {
+		if ((*aircraft[i]).getTailcode() == tailcode) {
+			aircraft[i] = aircraft[aircraft.size() - 1];
+			aircraft.pop_back(tailcode);
+		}
+}
 }
 
 bool AircraftCollection::hasAicraft(string tailcode)
 {
+	for (int z = 0; z < aircraft.size(); z++) {
+		if ((*aircraft).getTailcode == tailcode) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	return false;
 }
 
-const void AircraftCollection::getAircraft(string tailcode)
+const string AircraftCollection::getAircraft(string tailcode)
 {
-	return void();
+	for (int k = 0; k < aircraft.size(); k++) {
+		if ((*aircraft[k]).getTailcode() == tailcode) {
+			return tailcode;
+		}
+	}
 }
 
-const string AircraftCollection::getAircraft()
+const void AircraftCollection::getAircraft()
 {
-	return string();
+	for (int j = 0; j < aircraft.size(); j++) {
+		cout << aircraft[j] << endl;
+	}
 }
 
 
