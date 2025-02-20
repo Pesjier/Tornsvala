e382d54ca026c7f4d8b7b9aabf4ad1ffd28e2c5b
naveedrasooly <73232441+naveedrasooly@users.noreply.github.com>
2025-02-20 12:39:28 +0100

    Add files via upload

---
+++ Aircraft.cpp
@@ -0,0 +1,25 @@
+#include "Aircraft.h"
+
+Aircraft::Aircraft(const string& getTailcode)
+{
+}
+
+Aircraft::Aircraft(const string& getInfo)
+{
+}
+
+Aircraft::Aircraft(const string& getType)
+{
+}
+
+void Aircraft::setLastSpotted(Date date)
+{
+}
+
+void Aircraft::setLastNotified(Date date)
+{
+}
+
+void Aircraft::setRouteAndDest(RouteAndPredictedDest routeAndDest)
+{
+}
---
+++ Aircraft.h
@@ -0,0 +1,24 @@
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Aircraft {
+private:
+    string tailCode;
+    string type;
+    Date lastSpotted;
+    Date lastNotified;
+    RouteAndPredictedDest routeAndDest;
+
+public:
+    Aircraft(const string& getTailcode);
+    Aircraft(const string& getType);
+    Aircraft(const string& getInfo);
+
+    void setLastSpotted(Date date);
+    void setLastNotified(Date date);
+    void setRouteAndDest(RouteAndPredictedDest routeAndDest);
+
+};
+
